87b7e2c1af668e21447339c585c41157dc5f02c0
Nikolay Gavrilyonok <Nikolay.Gavrilyonok@gmai.com>
2019-06-28 23:22:59 +0300

    constants added

---
+++ src/constants/constants.js
@@ -0,0 +1,48 @@
+export const CONFIG_TRANSACTIONS_TABLE_HEADERS = [
+  {
+    dataSourceKey: 'title',
+    headerTitle: 'Title'
+  },
+  {
+    dataSourceKey: 'description',
+    headerTitle: 'Description'
+  },
+  {
+    dataSourceKey: 'category',
+    headerTitle: 'Category'
+  },
+  {
+    dataSourceKey: 'date',
+    headerTitle: 'Date'
+  },
+  {
+    dataSourceKey: 'amount',
+    headerTitle: 'Amount'
+  }
+];
+
+export const DATA_SOURCE_AMOUNT = 'amount';
+
+export const DATA_SOURCE_DATE = 'date';
+
+export const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+export const STYLES = {
+  border: 'solid 1px',
+  padding: '4px'
+};
+
+export const WEEK_DURATION_MS = 1000 * 60 * 60 * 24 * 7;
