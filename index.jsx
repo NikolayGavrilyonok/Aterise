e666f16abb6922387d68b9a23cb318e04ca89d89
Nikolay.Gavrilyonok <Nikolay.Gavrilyonok@service-it.by>
2019-06-29 17:05:57 +0300

    index.jsx edited

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {mockData} from './src/constants/mock';
+import {mockData} from './src/mock/mockData';
 import App from './src/core/App';
 
 render(<App data={mockData}/>, document.getElementById('app-root'));
